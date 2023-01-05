bcdd655bcf98cfb3ea62ae25c108f0bcf4e8286e
PixelGameWizard <vdarkest.gamer@gmail.com>
2023-01-05 00:44:06 -0500

    MNT: Remove deprecated calls to set render transparency

--- src/main/java/pixelgamewizard/pgwbandedtorches/common/BandedTorchesMod.java
+++ src/main/java/pixelgamewizard/pgwbandedtorches/common/BandedTorchesMod.java
@@ -1,12 +1,9 @@
 package pixelgamewizard.pgwbandedtorches.common;
 
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
@@ -17,20 +14,9 @@ public class BandedTorchesMod
 
     public BandedTorchesMod()
     {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::handleCreativeModeTabEvent);
         MinecraftForge.EVENT_BUS.register(this);
         RegistrationHandler.init();
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event)
-    {
-        for (int torchIndex = 0; torchIndex < Constants.TORCH_COUNT; torchIndex++)
-        {
-            ModBlocks.TorchBlockRegistryObjects torch = ModBlocks.torches[torchIndex];
-            ItemBlockRenderTypes.setRenderLayer(torch.block.get(), RenderType.cutout());
-            ItemBlockRenderTypes.setRenderLayer(torch.wallBlock.get(), RenderType.cutout());
-        }
     }
 
     private void handleCreativeModeTabEvent(final CreativeModeTabEvent.BuildContents event)
