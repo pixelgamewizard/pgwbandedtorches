20759e981a50758f92983f441b33111fc37674f9
PixelGameWizard <vdarkest.gamer@gmail.com>
2023-12-29 09:11:50 -0500

    MNT: I correct my prior comment. Remove event bus registration from main mod file. Thanks XFactHD for the tip

--- src/main/java/pixelgamewizard/pgwbandedtorches/common/BandedTorchesMod.java
+++ src/main/java/pixelgamewizard/pgwbandedtorches/common/BandedTorchesMod.java
@@ -1,10 +1,6 @@
 package pixelgamewizard.pgwbandedtorches.common;
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 
 @Mod(BandedTorchesMod.MODID)
@@ -12,15 +8,8 @@ public class BandedTorchesMod
 {
     public static final String MODID = "pgwbandedtorches";
 
-    public BandedTorchesMod(IEventBus modEventBus)
+    public BandedTorchesMod()
     {
-        NeoForge.EVENT_BUS.register(BandedTorchesMod.class);
         RegistrationHandler.init();
     }
-
-    // For some reason the NeoForge API introduced a requirement that mod classes
-    // implement a SubscribeEvent, even if there are event handlers elsewhere.
-    // https://neoforged.net/news/20.2eventbus-changes/
-    @SubscribeEvent
-    private static void dummyFunction(final BuildCreativeModeTabContentsEvent ignored) {}
 }
